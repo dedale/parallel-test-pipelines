a4e8785b0a9a2b185f9229d0fbd0229315302f20
dedale <ded@laposte.net>
2020-10-18 14:31:55 +0200

    [Tests] Proper PowerShell

--- test.ps1
+++ test.ps1
@@ -1,4 +1,46 @@
 
-.build\bin\netcoreapp3.1\NUnitAdapter.exe $($env:SYSTEM_ARTIFACTSDIRECTORY)\bin\netcoreapp3.1
+<#
+.SYNOPSIS
+Run all tests.
 
-.build\bin\net48\NUnitAdapter.exe $($env:SYSTEM_ARTIFACTSDIRECTORY)\bin\net48
+.DESCRIPTION
+Run .NET Core 3.1 and .NET Framework 4.8 NUnit tests for all assemblies in artifact.
+#>
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+function ExitWithCode
+{
+    param
+    (
+        [Parameter()]
+        $exitCode
+    )
+
+    $host.SetShouldExit($exitCode)
+    exit
+}
+
+if (-not (Test-Path Env:BUILD_SOURCESDIRECTORY)) {
+    Write-Error "BUILD_SOURCESDIRECTORY undefined"
+    ExitWithCode 1
+}
+if (-not (Test-Path Env:SYSTEM_ARTIFACTSDIRECTORY)) {
+    Write-Error "SYSTEM_ARTIFACTSDIRECTORY undefined"
+    ExitWithCode 1
+}
+
+try {
+    Invoke-Expression "${Env:BUILD_SOURCESDIRECTORY}\.build\bin\netcoreapp3.1\NUnitAdapter.exe ${Env:SYSTEM_ARTIFACTSDIRECTORY}\bin\netcoreapp3.1"
+} catch {
+    ExitWithCode 1
+}
+
+try {
+    Invoke-Expression "${Env:BUILD_SOURCESDIRECTORY}\.build\bin\net48\NUnitAdapter.exe ${Env:SYSTEM_ARTIFACTSDIRECTORY}\bin\net48"
+} catch {
+    ExitWithCode 1
+}
+
+ExitWithCode 0
