1401803a3c533c496d7b5702d12f16829cd35a09
dedale <ded@laposte.net>
2020-10-18 13:45:20 +0200

    [Azure Pipeline] Run tests

--- src/NUnitAdapter/Prod/Program.cs
+++ src/NUnitAdapter/Prod/Program.cs
@@ -9,17 +9,30 @@ namespace NUnitAdapter
 {
     class Program
     {
+        private sealed class Listener : ITestEventListener
+        {
+            public void OnTestEvent(string report)
+            {
+            }
+        }
+        private static int CompareFixtures(string x, string y)
+        {
+            var xid = x.Split(new[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries).Last();
+            var yid = y.Split(new[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries).Last();
+            if (int.TryParse(xid, out var xi) && int.TryParse(yid, out var yi))
+                return xi.CompareTo(yi);
+            return x.CompareTo(y);
+        }
         static void Main(string[] args)
         {
             using (var engine = TestEngineActivator.CreateInstance())
             {
-#if NETFRAMEWORK
-                var binPath = args.FirstOrDefault() ?? @"D:\prog\git\hub\dedale\dummy-for-pipelines\.build\bin\net48";
-#endif
 #if NETCOREAPP
                 var binPath = args.FirstOrDefault() ?? @"D:\prog\git\hub\dedale\dummy-for-pipelines\.build\bin\netcoreapp3.1";
 #endif
-
+#if NETFRAMEWORK
+                var binPath = args.FirstOrDefault() ?? @"D:\prog\git\hub\dedale\dummy-for-pipelines\.build\bin\net48";
+#endif
                 engine.WorkDirectory = binPath;
                 engine.Initialize();
 
@@ -32,10 +45,9 @@ namespace NUnitAdapter
                     package.AddSetting("DomainUsage", "None");
 #endif
 #if NETFRAMEWORK
-                    package.AddSetting("DomainUsage", "Single");
+                    package.AddSetting("DomainUsage", "Single"); // Single
                     package.AddSetting("BasePath", binPath);
 #endif
-                    package.AddSetting("DisposeRunners", true);
                     try
                     {
                         using (var runner = engine.GetRunner(package))
@@ -45,6 +57,16 @@ namespace NUnitAdapter
                             doc.XPathSelectElements("//test-run")
                                 .ToList()
                                 .ForEach(e => Console.WriteLine($"Explored {e.Attribute("name").Value}, found {e.Attribute("testcasecount").Value} test(s)"));
+                            var fixtures = doc.XPathSelectElements("//test-suite[@type='TestFixture']")
+                                .Select(e => e.Attribute("fullname").Value)
+                                .ToList();
+                            fixtures.Sort(CompareFixtures);
+                            fixtures.Reverse();
+                            foreach (var fixture in fixtures)
+                            {
+                                Console.WriteLine($"Running {fixture}...");
+                                xml = runner.Run(new Listener(), TestFilter.Empty);
+                            }
                             runner.Unload();
                         }
                     }
---
+++ test.ps1
@@ -0,0 +1,4 @@
+
+.build\bin\netcoreapp3.1\NUnitAdapter.exe bin\netcoreapp3.1
+
+.build\bin\net48\NUnitAdapter.exe bin\net48
